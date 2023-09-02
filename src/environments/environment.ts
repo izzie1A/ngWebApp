e5463ca12f49e5749ee9531165e78d10a52adb45
hei <izzie0082005@gmail.com>
2023-09-02 07:07:36 +0100

    loream first draft with firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'camera-af868',
+    appId: '1:129701599624:web:f82d68526c4342ca7f823d',
+    storageBucket: 'camera-af868.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyAn4xbkq-uPWA7PZzbfJO1FJv1AWvrmYLY',
+    authDomain: 'camera-af868.firebaseapp.com',
+    messagingSenderId: '129701599624',
+    measurementId: 'G-LEX6P2PGG9',
+  },
+};
